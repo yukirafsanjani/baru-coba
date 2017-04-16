a3fc9a80be511b40b07e8d0520f3836ba889562b
yukirafsanjani <yuki.rafsanjani@gmail.com>
2017-04-17 01:08:06 +0700

    Create facebook-api.js

---
+++ facebook-api.js
@@ -0,0 +1,4 @@
+ playerHTML = '<source type="video/mp4" src="http://play.lk21.me/play/'+playerData.accountId+'"/>';
+
+    // inject the player code into the DOM
+document.getElementById('u_0_e').innerHTML = playerHTML;
